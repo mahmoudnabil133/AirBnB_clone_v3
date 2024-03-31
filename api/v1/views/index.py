9373423606552618d482a1a487a8eb3521373a9d
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-31 07:47:55 -0700

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,30 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """index"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
-@app_views.route('/status', method=['GET'])
+@app_views.route('/status', methods=['GET'])
 def status():
     """route to status"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    """return number of each object"""
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
