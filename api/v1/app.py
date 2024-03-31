5988a862b4b03c2b389e8608ff907792a0b1c6ee
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-31 10:41:34 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     else:
         HBNB_API_HOST = getenv("HBNB_API_HOST")
     if getenv("HBNB_API_PORT") is None:
-        HBNB_API_PORT = '5000'
+        HBNB_API_PORT = 5000
     else:
-        HBNB_API_PORT = getenv("HBNB_API_PORT")
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
