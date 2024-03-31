ac8445314af74a2fc6699db33df5bf687017fa93
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-31 08:40:59 -0700

    stats

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 app.url_map.strict_slashes = False
