09b084e7bccba68607dc0a272aebed07523347dc
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-04-01 13:02:54 -0700

    app

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,7 @@
 """adding blueprint to app_views"""
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 
 from api.v1.views.index import *  # noqa
 from api.v1.views.states import *  # noqa
