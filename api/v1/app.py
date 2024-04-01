f6f89b4d1adcace31a7ca15464a096697771add7
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-04-01 13:08:45 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """checking and closing app"""
-from flask import Flask, Blueprint, make_response, jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -9,6 +9,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
