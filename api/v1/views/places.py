96a170364068841f5df100b3f70760f276cc21ab
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-04-01 12:10:35 -0700

    users

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,55 +1,62 @@
 #!/usr/bin/python3
-"""users"""
+"""places"""
 from api.v1.views import app_views
 from flask import request, abort, jsonify
 from models import storage
-from models.user import User
+from models.city import City
+from models.place import Place
 from datetime import datetime
 import uuid
 
 
-@app_views.route('/users/', methods=['GET'])
-@app_views.route('/users', methods=['GET'])
-def list_users():
-    """list users"""
-    list_users = [obj.to_dict() for obj in storage.all(User).values()]
-    return jsonify(list_users)
-
-
-@app_views.route('/users/<user_id>', methods=['GET'])
-def get_user(user_id):
-    """get user"""
-    all_users = storage.all(User).values()
-    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
-    if user_obj == []:
+@app_views.route('/cities/<city_id>/places', methods=['GET'])
+@app_views.route('/cities/<city_id>/places/', methods=['GET'])
+def list_places_of_city(city_id):
+    """list places of city"""
+    all_cities = storage.all(City).values()
+    city_obj = [obj.to_dict() for obj in all_cities if obj.id == city_id]
+    if city_obj == []:
         abort(404)
-    return jsonify(user_obj[0])
+    list_places = [obj.to_dict() for obj in storage.all(Places).vlaues(),
+                   if city_id == obj.city_id]
+    return jsonify(list_places)
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    """delete user"""
-    all_users = storage.all(User).values()
-    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
-    if user_obj == []:
+@app_views.route('/places/<place_id>', methods=['GET'])
+def get_place(place_id):
+    """get place"""
+    all_places = storage.all(Place).values()
+    place_obj = [obj.to_dict() for obj in all_places if obj.id == place_id]
+    if place_obj == []:
         abort(404)
-    user_obj.remove(user_obj[0])
-    for obj in all_users:
-        if obj.id == user_id:
+    return jsonify(place_obj[0])
+
+
+@app_views.route('/place/<place_id>', methods=['DELETE'])
+def delete_place(place_id):
+    """delete place"""
+    all_places = storage.all(Place).values()
+    place_obj = [obj.to_dict() for obj in all_places
+                 if obj.id == place_id]
+    if place_obj == []:
+        abort(404)
+    place_obj.remove(place_obj[0])
+    for obj in all_places:
+        if obj.id == place_id:
             storage.delete(obj)
             storage.save()
     return jsonify({}), 200
 
 
-@app_views.route('/users/', methods=['POST'])
-def create_user():
-    """create user"""
+@app_views.route('/cities/<city_id>/places', methods=['POST'])
+def create_city(city_id):
+    """create city"""
     if not request.get_json():
         abort(400, 'Not a JSON')
-    if 'email' not in request.get_json():
-        abort(400, 'Missing email')
+    if 'user_id' not in request.get_json():
+        abort(400, 'Missing name')
     if 'password' not in request.get_json():
-        abort(400, 'Missing password')
+        abort(400, 'Missing name')
     users = []
     new_user = User(email=request.json['email'],
                     password=request.json['password'])
