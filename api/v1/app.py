a0ab37da6cb6ba3aca519f5a266b3e1794bce8bd
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-04-01 13:39:29 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,13 +27,13 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-if __name__ == "__main__":
-    if getenv('HBNB_API_HOST') is None:
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
         HBNB_API_HOST = '0.0.0.0'
     else:
-        HBNB_API_HOST = getenv('HBNB_API_HOST')
-    if getenv('HBNB_API_PORT') is None:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
         HBNB_API_PORT = 5000
     else:
-        HBNB_API_PORT = int(getenv('HBNB_API_PORT'))
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
