12b672fd7568f71071d203a876fb62a432beb5cf
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-04-01 12:45:08 -0700

    places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -54,46 +54,70 @@ def create_city(city_id):
     if not request.get_json():
         abort(400, 'Not a JSON')
     if 'user_id' not in request.get_json():
+        abort(400, 'Missing user_id')
+    if 'name' not in request.get_json():
         abort(400, 'Missing name')
-    if 'password' not in request.get_json():
-        abort(400, 'Missing name')
-    users = []
-    new_user = User(email=request.json['email'],
-                    password=request.json['password'])
-    storage.new(new_user)
-    storage.save()
-    users.append(new_user.to_dict())
-    return jsonify(users[0]), 201
-
-
-@app_views.route('/users/<user_id>', methods=['PUT'])
-def updates_user(user_id):
-    """update user"""
+    all_cities = storage.all(City).values()
+    city_obj = [obj.to_dict() for obj in all_cities
+                 if obj.id == city_id]
+    if city_obj == []:
+        abort(404)
+    places = []
+    new_place = User(name=request.json['name'],
+                    user_id=request.json['user_id'], city_id=city_id)
     all_users = storage.all(User).values()
-    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    user_obj = [obj.to_dict() for obj in all_users
+                 if obj.id == new_place.user_id]
     if user_obj == []:
+        abort(404)
+    storage.new(new_place)
+    storage.save()
+    places.append(new_place.to_dict())
+    return jsonify(places[0]), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'])
+def updates_place(place_id):
+    """update user"""
+    all_places = storage.all(Place).values()
+    place_obj = [obj.to_dict() for obj in all_places if obj.id == place_id]
+    if place_obj == []:
         abort(404)
     if not request.get_json():
         abort(400, 'Not a JSON')
-    try:
-        user_obj[0]['first_name'] = request.json['first_name']
-    except Exception as error:
-        pass
-    try:
-        user_obj[0]['last_name'] = request.json['last_name']
-    except Exception as error:
-        pass
-    for obj in all_users:
-        if obj.id == user_id:
-            try:
-                if request.json['first_name'] is not None:
-                    obj.first_name = request.json['first_name']
-            except Exception as error:
-                pass
-            try:
-                if request.json['last_name'] is not None:
-                    obj.last_name = request.json['last_name']
-            except Exception as error:
-                pass
+    if 'name' in request.get_json():
+        place_obj[0]['name'] = request.json['name']
+    if 'description' in request.get_json():
+        place_obj[0]['description'] = request.json['description']
+    if 'number_rooms' in request.get_json():
+        place_obj[0]['number_rooms'] = request.json['number_rooms']
+    if 'number_bathrooms' in request.get_json():
+        place_obj[0]['number_bathrooms'] = request.json['number_bathrooms']
+    if 'max_guest' in request.get_json():
+        place_obj[0]['max_guest'] = request.json['max_guest']
+    if 'price_by_night' in request.get_json():
+        place_obj[0]['price_by_night'] = request.json['price_by_night']
+    if 'latitude' in request.get_json():
+        place_obj[0]['latitude'] = request.json['latitude']
+    if 'longitude' in request.get_json():
+        place_obj[0]['longitude'] = request.json['longitude']
+    for obj in all_places:
+        if obj.id == place_id:
+            if 'name' in request.get_json():
+                obj.name = request.json['name']
+            if 'description' in request.get_json():
+                obj.description = request.json['description']
+            if 'number_rooms' in request.get_json():
+                obj.number_rooms = request.json['number_rooms']
+            if 'number_bathrooms' in request.get_json():
+                obj.number_bathrooms = request.json['number_bathrooms']
+            if 'max_guest' in request.get_json():
+                obj.max_guest = request.json['max_guest']
+            if 'price_by_night' in request.get_json():
+                obj.price_by_night = request.json['price_by_night']
+            if 'latitude' in request.get_json():
+                obj.latitude = request.json['latitude']
+            if 'longitude' in request.get_json():
+                obj.longitude = request.json['longitude']
     storage.save()
-    return jsonify(user_obj[0]), 200
+    return jsonify(place_obj[0]), 200
